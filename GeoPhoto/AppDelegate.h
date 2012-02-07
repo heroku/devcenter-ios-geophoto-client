e473a1a3144485cc8820b8a79b83e0a80d554c61
Mattt Thompson <m@mattt.me>
2012-02-07 14:59:43 -0800

    Initial Commit

---
+++ GeoPhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeoPhoto
+//
+//  Created by Mattt Thompson on 12/02/07.
+//  Copyright (c) 2012年 Heroku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GeoPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GeoPhoto
+//
+//  Created by Mattt Thompson on 12/02/07.
+//  Copyright (c) 2012年 Heroku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
